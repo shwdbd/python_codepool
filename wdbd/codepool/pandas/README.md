486aa5b85436d67445e481334f629a6cd3e05478
shwdbd <shwangjj@163.com>
2019-10-21 10:47:58 +0800

    升级基础包

--- wdbd/codepool/pandas/README.md
+++ wdbd/codepool/pandas/README.md
@@ -10,7 +10,7 @@
 | df_basic.py          | DF的构建与基本属性 ok              |
 | df_selection.py      | DF的条件选择  ok                     |
 | df_v_op.py           | DF的向量运算（不同形状的df加减） |
-| df_col_raname.py     | df列改名                         |
+| df_col_raname.py     | df列改名    ok                     |
 | df_null.py           | 空值的处理                       |
 | df_apply_func.py     | apply函数                        |
 | df_daterange_func.py | pd.daterange示例                 |
--- wdbd/codepool/pandas/df_null.py
+++ wdbd/codepool/pandas/df_null.py
@@ -7,6 +7,26 @@
 @Version :   1.0
 @Contact :   shwangjj@163.com
 @Desc    :   空值的处理
-'''
 
-# here put the import lib
+df.dropna 按行删除
+df.isnull
+df.fillna
+df.
+
+https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.dropna.html#pandas.DataFrame.dropna
+
+
+'''
+import pandas as pd
+
+
+def get_df():
+    df = pd.DataFrame({"name": ['Alfred', 'Batman', 'Catwoman'],
+                    "toy": [np.nan, 'Batmobile', 'Bullwhip'],
+                    "born": [pd.NaT, pd.Timestamp("1940-04-25"),
+                             pd.NaT]})
+    return df
+
+
+
+
