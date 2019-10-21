040ca2b94c3437ae8ce3133273731359df0f691a
shwdbd <shwangjj@163.com>
2019-10-21 14:58:58 +0800

    提交部分代码

--- wdbd/codepool/pandas/df_daterange_func.py
+++ wdbd/codepool/pandas/df_daterange_func.py
@@ -6,9 +6,23 @@
 @Author  :   Jeffrey Wang
 @Version :   1.0
 @Contact :   shwangjj@163.com
-@Desc    :   pd.daterange示例
+@Desc    :   pd.date_range示例
+
+date_range是pd的全局函数
+
+DOC:
+https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.date_range.html#pandas.date_range
 
 
 '''
+import pandas as pd
 
-# here put the import lib
+
+if __name__ == "__main__":
+    # 生成日期区间，前闭后闭：
+    print(pd.date_range(start='1/1/2018', end='1/08/2018'))
+    # DatetimeIndex(['2018-01-01', '2018-01-02', '2018-01-03', '2018-01-04',
+    #           '2018-01-05', '2018-01-06', '2018-01-07', '2018-01-08'],
+    #          dtype='datetime64[ns]', freq='D')
+
+    print(pd.date_range(start='1/1/2018', end='1/08/2018', periods=3))  # 每隔3天
--- wdbd/codepool/pandas/df_sort.py
+++ wdbd/codepool/pandas/df_sort.py
@@ -8,7 +8,45 @@
 @Contact :   shwangjj@163.com
 @Desc    :   df排序
 
+1. sort_by_columns      按指定列排序 sort_values, sort_index
+
+
 
 '''
+import pandas as pd
+import numpy as np
 
-# here put the import lib
+
+def get_df():
+    df = pd.DataFrame({
+        'col1': ['A', 'A', 'B', np.nan, 'D', 'C'],
+        'col2': [2, 1, 9, 8, 7, 4],
+        'col3': [0, 1, 9, 4, 2, 3], })
+    return df
+
+
+def sort_by_columns():
+    """
+    df排序
+
+    有两个函数：sort_by_values, sort_by_index
+
+    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.sort_values.html#pandas.DataFrame.sort_values
+    https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.sort_index.html#pandas.DataFrame.sort_index
+
+    """
+    df = get_df()
+    print(df)
+
+    # 简单按列排序, np.NaN是最后
+    print(df.sort_values(by='col1'))
+
+    # 多列排序 ,col1升序，col2降序
+    print(df.sort_values(by=['col1', 'col2'], ascending=[True, False]))
+
+    # 按索引排序
+    print(df.sort_index(ascending=False))
+
+
+if __name__ == "__main__":
+    sort_by_columns()
