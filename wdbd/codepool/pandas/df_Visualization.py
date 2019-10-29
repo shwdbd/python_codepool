5414f48b94e243ed2c59bba9fc734f77adeec316
shwdbd <shwangjj@163.com>
2019-10-29 17:56:22 +0800

    提交图形Demo

--- wdbd/codepool/pandas/df_Visualization.py
+++ wdbd/codepool/pandas/df_Visualization.py
@@ -25,7 +25,6 @@ https://pandas.pydata.org/pandas-docs/stable/reference/frame.html#plotting
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import tushare as ts
 
 
 def simple_plot():
@@ -54,8 +53,18 @@ def demo_stock():
     """
     # 构建
     # 使用 date_range构建一个index，然后用 随机函数构建数据
-    pass
-    
+    list_l = [[1, 3, 3, 5, 4], [11, 7, 15, 13, 9], [4, 2, 7, 9, 3], [15, 11, 12, 6, 11]]
+    date_range = pd.date_range(start="20180701", periods=4)
+    df = pd.DataFrame(list_l, index=date_range, columns=['a', 'b', 'c', 'd', 'e'])
+    print(df)
+
+    df_ax = df.plot(figsize=(8, 4))
+    # print(type(df_ax))  # <class 'matplotlib.axes._subplots.AxesSubplot'>
+    plt.xlabel("time")
+    plt.ylabel("num")
+    plt.legend(loc="best")
+    plt.scatter(df.index, df["a"], c="red", marker="*")  # 这个例子中，散点图是在 plt.legend 命令之后画的，所以不会出现在图例中（对比下面的例子理解）。
+    plt.show()
 
 
 if __name__ == "__main__":
