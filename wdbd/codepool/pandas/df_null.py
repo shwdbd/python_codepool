fef805a191defd5470312a88af35d936b893d068
shwdbd <shwangjj@163.com>
2019-11-11 20:36:57 +0800

    df.fillna演示代码

--- wdbd/codepool/pandas/df_null.py
+++ wdbd/codepool/pandas/df_null.py
@@ -11,11 +11,22 @@
 1. demo_dropna      df.dropna 删除空值的数据
 2. demo_isna_notsa  空值判断
 3. demo_fillna      控制填充
+4. demo_fillna_method   演示fillna不同method的用法
+5. demo_fillna_dict     演示使用dict按列使用不同值替换
 
 dropna:
 https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.dropna.html#pandas.DataFrame.dropna
-fillna:
+
+fillna:(value=None, method=None, axis=None, inplace=False, limit=None, downcast=None)
 https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.fillna.html#pandas.DataFrame.fillna
+- method:
+    - backfill/bfill    后一行填充
+    - ffill/pad         前一行的值填充
+    - None  指定值填充
+- limit: 替换N个后停止
+TODO 使用dict，按列填充不同的值
+
+
 isna:
 https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.isna.html#pandas.DataFrame.isna
 nasa:
@@ -78,5 +89,69 @@ def demo_fillna():
     print(df.fillna(value=values))
 
 
+def demo_fillna_method():
+    """
+    演示 fillna 不同method参数下的情况
+    """
+    df = pd.DataFrame([[np.nan, 2, np.nan, 0],
+                       [3, 4, np.nan, 1],
+                       [np.nan, np.nan, np.nan, 5],
+                       [np.nan, 3, np.nan, 4]],
+                      columns=list('ABCD'))
+    print(df)
+    #      A    B   C  D
+    # 0  NaN  2.0 NaN  0
+    # 1  3.0  4.0 NaN  1
+    # 2  NaN  NaN NaN  5
+    # 3  NaN  3.0 NaN  4
+
+    # method = ffill
+    print('fillna.method = padfill : ')      # 前一行填充后一行
+    print(df.fillna(method='pad'))
+    #      A    B    C    D
+    # 0  NaN  2.0 NaN  0
+    # 1  3.0  4.0 NaN  1
+    # 2  3.0  4.0 NaN  5
+    # 3  3.0  3.0 NaN  4
+    print('fillna.method = ffill, axis=columns : ')
+    print(df.fillna(method='ffill', axis='columns'))    # 前一列填充后一列
+    #      A    B    C    D
+    # 0  NaN  2.0  2.0  0.0
+    # 1  3.0  4.0  4.0  1.0
+    # 2  NaN  NaN  NaN  5.0
+    # 3  NaN  3.0  3.0  4.0
+
+    # method = bfill
+    print('fillna.method = bfill : ')      # 后一行填充后一行
+    print(df.fillna(method='bfill'))
+    #  A    B   C  D
+    # 0  3.0  2.0 NaN  0
+    # 1  3.0  4.0 NaN  1
+    # 2  NaN  3.0 NaN  5
+    # 3  NaN  3.0 NaN  4
+
+
+def demo_fillna_dict():
+    """演示使用dict按列使用不同值替换
+    """
+    df = pd.DataFrame([[np.nan, 2, np.nan, 0],
+                       [3, 4, np.nan, 1],
+                       [np.nan, np.nan, np.nan, 5],
+                       [np.nan, 3, np.nan, 4]],
+                      columns=list('ABCD'))
+    print(df)
+    #      A    B   C  D
+    # 0  NaN  2.0 NaN  0
+    # 1  3.0  4.0 NaN  1
+    # 2  NaN  NaN NaN  5
+    # 3  NaN  3.0 NaN  4
+    print(df.fillna({'A': 'a', 'B': 'b', 'C': 'c', 'D': 'd'}))
+    #    A  B  C  D
+    # 0  a  2  c  0
+    # 1  3  4  c  1
+    # 2  a  b  c  5
+    # 3  a  3  c  4
+
+
 if __name__ == "__main__":
-    demo_fillna()
+    demo_fillna_dict()
